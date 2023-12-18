e0041e77915dd40905d1319dba3309650e187da1
tarikulat <tarikulat124@gmail.com>
2023-12-18 11:45:53 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+            <RouterLink class="navbar-brand" to="#">Navbar</RouterLink>
+            <div class="collapse navbar-collapse">
+              <div class="navbar-nav">
+                <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
+                <RouterLink class="nav-link" to="/contact">Features</RouterLink>
+                <RouterLink class="nav-link" to="/about">Pricing</RouterLink>
+
+              </div>
+            </div>
+          </div>
+        </nav>
+        
+
+<!-- 
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/contact">Contact</RouterLink>
+      </nav> -->
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
---
+++ src/views/ContactView.vue
@@ -0,0 +1,5 @@
+<template>
+    <main>
+    <h1>This is Contact page</h1>
+    </main>
+  </template>
